538c4eb4650df5bde94631bafeadfea3387ae415
gregh13 <99034583+gregh13@users.noreply.github.com>
2022-08-18 17:23:17 +0700

    Delete 12.sql

--- 12.sql
+++
@@ -1,1 +0,0 @@
-SELECT DISTINCT(title) FROM movies WHERE title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = "Helena Bonham Carter") AND title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = "Johnny Depp");
