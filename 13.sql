f9ba9d9c7a2307e349e1a248ab28a8946df77143
gregh13 <99034583+gregh13@users.noreply.github.com>
2022-08-18 17:23:00 +0700

    Delete 13.sql

--- 13.sql
+++
@@ -1,1 +0,0 @@
-SELECT DISTINCT(name) FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id WHERE movies.title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = "Kevin Bacon") AND people.name != "Kevin Bacon";
